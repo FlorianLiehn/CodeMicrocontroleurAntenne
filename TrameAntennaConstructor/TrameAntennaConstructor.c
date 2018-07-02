a194623d4e1ee674d828f25af874406dc367ad93
Florian Liehn <florian@liehn.com>
2018-07-02 15:52:17 +0200

    Correct computeXY & add antenna mods

--- TrameAntennaConstructor/TrameAntennaConstructor.c
+++ TrameAntennaConstructor/TrameAntennaConstructor.c
@@ -33,12 +33,12 @@ void computeXYencoderFromAzEl(int16_t* X,int16_t* Y,double az,double el){
 	//calculate X
 	double x_angle=asin( cos(deg2Rad(el))*sin(deg2Rad(az)) );
 
-	*X= (rad2Deg( x_angle ) / ENCODER_X_ANGLE_IN_A_STEP);
+	*X= (rad2Deg( x_angle ) * ENCODER_X_STEP_BY_ANGLE);
 
 	//calculate Y
 	double y_angle=acos( sin(deg2Rad(el)) / cos(x_angle) );
 
-	*Y= (rad2Deg( y_angle ) / ENCODER_Y_ANGLE_IN_A_STEP);
+	*Y= (rad2Deg( y_angle ) * ENCODER_Y_STEP_BY_ANGLE);
 	if( az>90 && az<270 )
 		*Y=-*Y;
 
@@ -66,12 +66,13 @@ void computeAntennaMessage(char*message, uint8_t puissance,uint8_t mode,
 	int32_t X, Y;
 	char X_send[INT16_ASCCI_HEX_LENGTH];char Y_send[INT16_ASCCI_HEX_LENGTH];
 	computeXYencoderFromAzEl((int16_t*)&X, (int16_t*)&Y, az, el);
+
 	sprintf(X_send,"%04X", (uint16_t)(X &~ (0xFFFF0000) ) );
 	sprintf(Y_send,"%04X", (uint16_t)(Y &~ (0xFFFF0000) ) );
 
-	for(int i=INT16_ASCCI_HEX_LENGTH-1;i>=0;i--)
+	for(int i=0;i<INT16_ASCCI_HEX_LENGTH;i++)
 		*(message+(n++))=X_send[i];
-	for(int i=INT16_ASCCI_HEX_LENGTH-1;i>=0;i--)
+	for(int i=0;i<INT16_ASCCI_HEX_LENGTH;i++)
 		*(message+(n++))=Y_send[i];
 
 	*(message+(n++))=TAIL_ANTENNA[0];
