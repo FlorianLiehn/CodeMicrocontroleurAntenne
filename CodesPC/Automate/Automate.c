91bdef32f3870608fd32220095ba6ade1573600f
Florian Liehn <florian@liehn.com>
2018-07-16 17:46:52 +0200

    Upgrade Automate (first correct targeting)

--- CodesPC/Automate/Automate.c
+++ CodesPC/Automate/Automate.c
@@ -7,20 +7,39 @@
 
 #include "Automate.h"
 
-void loadTargetsFromFile(File_Target* targets,int* tot,
-		char* target_path,char*file_name){
-	targets[*tot]=(File_Target){};
-	//path
-	strcpy(targets[*tot].file,target_path);
-	strcat(targets[*tot].file,"/");
-	strcat(targets[*tot].file,file_name);
-	//extract time
-	//compute prio
-	printf("%s\n",targets[*tot].file);
-	(*tot)++;
+void updateTargetsTime(File_Target* targets,int*tot,time_t current_time,int*next){
+	//reini targets
+	*tot=0;*next=-1;
+	char missions[MAX_MISSION][MAX_FILE_NAME_LENGTH];
+	int nb_mission=readMissions(missions);
+
+	//read mission directory
+	for(int i=0;i<nb_mission;i++){
+			loadTargetsFromDir(targets,tot,missions[i],i,current_time,next);
+	}
 }
 
-void loadTargetsFromDir(File_Target* targets,int* tot ,char* mission_path){
+int readMissions(char missions[MAX_MISSION][MAX_FILE_NAME_LENGTH]){
+
+	int nb_mission=0;
+	char buf [MAX_FILE_NAME_LENGTH];
+	FILE *ptr_file =fopen(PRIO_FILE, "r");
+	while (fgets(buf,MAX_FILE_NAME_LENGTH, ptr_file)!=NULL){
+			//Skip commented line
+			if(buf[0]=='#')continue;
+
+			strcpy(missions[nb_mission],REP_SUIVI);
+			strcat(missions[nb_mission],buf);
+			//replace \n by /
+			missions[nb_mission][strlen(missions[nb_mission])-1]='/';
+			printf("Mission %d:%s\n",nb_mission,missions[nb_mission]);
+			nb_mission++;
+	}
+	return nb_mission;
+}
+
+void loadTargetsFromDir(File_Target* targets,int* tot ,char* mission_path,int prio,
+						time_t current_time,int*next){
 	//open directory
 	DIR*mission_dir=opendir(mission_path);
 	if(!mission_dir){
@@ -30,44 +49,129 @@ void loadTargetsFromDir(File_Target* targets,int* tot ,char* mission_path){
 	//read files
 	struct dirent*target_file;
 	while((target_file=readdir(mission_dir))!=NULL){
-		if(target_file->d_type==DT_REG && target_file->d_name[0]!='.')
-		loadTargetsFromFile(targets,tot,mission_path,target_file->d_name);
+		if(target_file->d_type==DT_REG && target_file->d_name[0]!='.'){
+			loadTargetsFromFile(targets,tot,mission_path,prio,
+					target_file->d_name,current_time,next);
+		}
 	}
 	closedir(mission_dir);
 }
 
-void updateTargetsTime(File_Target* targets,int*tot){
-	//reini targetes
-	*tot=0;
-	//open directory
-	DIR* missions_dir=opendir(REP_SUIVI);
-	if(!missions_dir){
-		fprintf(stderr,"Can't open dir:%s\n",REP_SUIVI);
-		exit(0);
-	}
-	//read mission directory
-	struct dirent*mission_cont;
-	while((mission_cont=readdir(missions_dir))!=NULL){
-		if(mission_cont->d_type==DT_DIR && mission_cont->d_name[0]!='.'){
-			char path[MAX_FILE_NAME_LENGTH];
-			strcpy(path,REP_SUIVI);
-			strcat(path,mission_cont->d_name);
-			loadTargetsFromDir(targets,tot,path);
+void loadTargetsFromFile(File_Target* targets,int* tot,char* target_path,int prio,
+						char*file_name,time_t current_time,int*next){
+	targets[*tot]=(File_Target){.priority_level=prio};
+	//path
+	strcpy(targets[*tot].file,target_path);
+	strcat(targets[*tot].file,file_name);
+	//extract time
+	extractTimeFromFile(targets[*tot].file,
+				&targets[*tot].beginning,
+				&targets[*tot].ending);
+	//check if near
+	double waiting_time=difftime(targets[*tot].beginning,current_time);
+	if( 0 < waiting_time && waiting_time < PREPARATION_TIME ){
+		printf("NEW:%f\n",difftime(targets[*tot].beginning,current_time));
+		//if no next or earlier
+		if(*next<0 || difftime(targets[*tot ].beginning,
+							   targets[*next].beginning) < 0 ){
+			*next=*tot;
 		}
 	}
-	closedir(missions_dir);
+	(*tot)++;
+}
+
+void extractTimeFromFile(char * path,time_t* begin,time_t* end){
+	//init
+	*begin=*end=0;
+	FILE *ptr_file =fopen(path, "r");
+
+	//read (first = begin date)
+	char buf[MAX_FILE_LINE_LENGTH];
+	char last_uncommented_line[MAX_FILE_LINE_LENGTH];
+	while (fgets(buf,MAX_FILE_LINE_LENGTH, ptr_file)!=NULL){
+		//Skip commented line
+		if(buf[0]=='#')continue;
+		else if (*begin==0){
+			extractTimeFromLine(buf,begin);
+		}
+		strcpy(last_uncommented_line,buf);
+	}
+	//read last uncommented line (end)
+	extractTimeFromLine(last_uncommented_line,end);
+}
+
+void extractTimeFromLine(char * line,time_t* unix_time){
+	*unix_time=0;
+	//scan date
+	struct tm date={};
+	sscanf(line,"%d-%d-%d %d:%d:%d",
+			&date.tm_year,&date.tm_mon,&date.tm_mday,
+			&date.tm_hour,&date.tm_min,&date.tm_sec);
+	//offset
+	date.tm_year-=TM_STRUCT_YEAR_OFFSET;
+	date.tm_mon-= TM_STRUCT_MONTH_OFFSET;
+	//compute
+	*unix_time=mktime(&date);
+}
+
+int checkTimeInTargetPeriod(time_t t,File_Target target){
+	//check > beginning && < ending
+	if(difftime(target.beginning,t)<PREPARATION_TIME &&
+	   difftime(t,   target.ending)<PREPARATION_TIME)
+		return 1;
+	return 0;
+}
+
+int checkTargetsCollision(File_Target tar1,File_Target tar2){
+	return  checkTimeInTargetPeriod(tar1.beginning,tar2) ||
+			checkTimeInTargetPeriod(tar1.ending   ,tar2) ||
+			checkTimeInTargetPeriod(tar2.beginning,tar1) ||
+			checkTimeInTargetPeriod(tar2.ending   ,tar1);
+}
+
+void checkForNextTargeting(File_Target* targets,int tot,int* next){
+	if(*next<0)return;
+	for(int i=0;i<tot;i++){
+		if(i!=*next && targets[i].priority_level<targets[*next].priority_level &&
+		 checkTargetsCollision(targets[i],targets[*next]) ){
+			//check the new date
+			time_t now=mktime(gmtime(&(time_t){time(NULL)}));
+			if(difftime(targets[i].beginning,now)<PREPARATION_TIME){
+				//re-check with the new targets
+				printf("Collision %d VS %d\n",*next,i);
+				*next=i;
+				return checkForNextTargeting(targets, tot,next);
+			}
+			//too early
+			*next=-1;
+			return;
+		}
+	}
 }
 
 void main(void){
 	//init variable
-	File_Target possible_targets[MAX_TARGETS];
-	int tot=0;
+	File_Target possibles_targets[MAX_TARGETS];
+	int tot,next;
 
 	//while(1){
-		updateTargetsTime(possible_targets,&tot);
-
-		//Check new suivi
-			//DO IT
+		//get current date & update targets
+		time_t now=mktime(gmtime(&(time_t){time(NULL)}));
+		updateTargetsTime(possibles_targets,&tot,now,&next);
+		//Check if next targeting is correct
+		checkForNextTargeting(possibles_targets,tot,&next);
+		if(next>=0){
+	    	printf("Next :%s\n",possibles_targets[next].file);
+	    	//do the targeting
+	    	targetingFromFile(possibles_targets[next].file);
+	    	//sleep(=> unactivate until end of targetting)
+			now=mktime(gmtime(&(time_t){time(NULL)}));
+			sleep( MAX(difftime(possibles_targets[next].ending,now),0) );
+		}
+		else{
+	    	printf("No next targets\n");
+	    	//sleep(CHECKING_LOOP_TIME);
+		}
 	//}
-
+	printf("Exit\n");
 }
--- CodesPC/Automate/Automate.h
+++ CodesPC/Automate/Automate.h
@@ -9,26 +9,42 @@
 #define CODESPC_AUTOMATE_AUTOMATE_H_
 
 #include "../PcParameters.h"
+#include <time.h>
 #include <dirent.h>
+#include <inttypes.h>
 
 #define REP_SUIVI "../../Fichiers_de_designations/"
+#define PRIO_FILE REP_SUIVI"Prio.txt"
 #define REP_CORTEX_CONFIG ???? //TODO connect to cortex IHM
 
-#define PREPARATION_TIME 15min
+#define PREPARATION_TIME 15*60	//15 min
+#define CHECKING_LOOP_TIME 10	//10 s
 
 #define MAX_FILE_NAME_LENGTH 1024
+#define MAX_MISSION 50
 #define MAX_TARGETS 1000
+#define TM_STRUCT_YEAR_OFFSET 1900
+#define TM_STRUCT_MONTH_OFFSET 1
 typedef struct {
 	char file[MAX_FILE_NAME_LENGTH];
-	uint64_t beginning;
-	uint64_t ending;
+	time_t beginning;
+	time_t ending;
 	int priority_level;
 }File_Target;
 
-void loadTargetsFromFile(File_Target* targets,int* tot,
-		char* target_path,char*file_name);
-void loadTargetsFromDir(File_Target* targets,int* tot ,char* mission_path);
-void updateTargetsTime(File_Target* targets,int*tot);
-void main(void);
+//update the targets
+void updateTargetsTime(File_Target* targets,int*tot,time_t current_time,int*next);
+int readMissions(char missions[MAX_MISSION][MAX_FILE_NAME_LENGTH]);
+void loadTargetsFromDir(File_Target* targets,int* tot ,char* mission_path,int prio,
+						time_t current_time,int*next);
+void loadTargetsFromFile(File_Target* targets,int* tot,char* target_path,int prio,
+						char*file_name,time_t current_time,int*next);
+void extractTimeFromFile(char * path,time_t* begin,time_t* end);
+void extractTimeFromLine(char * line,time_t* unix_time);
+
+//Check target & targeting
+int checkTimeInTargetPeriod(time_t t,File_Target target);
+int checkTargetsCollision(File_Target tar1,File_Target tar2);
+void checkForNextTargeting(File_Target* targets,int tot,int* next);
 
 #endif /* CODESPC_AUTOMATE_AUTOMATE_H_ */
