7d1f8e4c9bba2ae7c63fb11cfbc0702c8ece4275
Florian Liehn <florian@liehn.com>
2018-07-18 13:24:58 +0200

    Correct Serial configuration (\r remapping)

--- CodesPC/ComServer/ComServer.c
+++ CodesPC/ComServer/ComServer.c
@@ -21,27 +21,27 @@ int setInterfaceAttribs (int fd, int speed, int parity, int should_block)
         cfsetispeed (&tty, speed);
 
         tty.c_cflag = (tty.c_cflag & ~CSIZE) | CS8;     // 8-bit chars
-        // disable IGNBRK for mismatched speed tests; otherwise receive break
-        // as \000 chars
-        tty.c_iflag &= ~IGNBRK;         // disable break processing
-        tty.c_lflag &= ~(ICANON | ECHO | ECHOE | ISIG);
-        								// no signaling chars, no echo,
-                                        // no canonical processing
-
-
-        tty.c_oflag = 0;                // no remapping, no delays
-
-        tty.c_cc[VMIN]  = should_block ? 1 : 0;
-        tty.c_cc[VTIME] = 5;            // 0.5 seconds read timeout
-
-        tty.c_iflag &= ~(IXON | IXOFF | IXANY); // shut off xon/xoff ctrl
-
         tty.c_cflag |= (CLOCAL | CREAD);// ignore modem controls,
                                         // enable reading
         tty.c_cflag &= ~(PARENB | PARODD);      // shut off parity
         tty.c_cflag |= parity;
         tty.c_cflag &= ~CSTOPB;
         tty.c_cflag &= ~CRTSCTS;
+
+        // disable IGNBRK for mismatched speed tests; otherwise receive break
+        // as \000 chars
+        tty.c_lflag &= ~(ICANON | ECHO | ECHOE | ISIG);
+        								// no signaling chars, no echo,
+                                        // no canonical processing
+
+        tty.c_iflag &= ~IGNBRK;         // disable break processing
+        tty.c_iflag &= ~(IXON | IXOFF | IXANY); // shut off xon/xoff ctrl
+        tty.c_iflag &= ~(ICRNL | INLCR | IGNCR); // not map \r to \n
+
+        tty.c_oflag = 0;                // no remapping, no delays
+
+        tty.c_cc[VMIN]  = should_block ? 1 : 0;
+        tty.c_cc[VTIME] = 5;            // 0.5 seconds read timeout
 
         if (tcsetattr (fd, TCSANOW, &tty) != 0)
         {
